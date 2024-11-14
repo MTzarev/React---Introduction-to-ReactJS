77eda99429839f73c61ca560f9eb4d28deba080e
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-14 16:16:31 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,74 @@
+function Main(){
+return(
+<main>
+        <section className="section-one">
+            <h2>Преподаватели</h2>
+           <p class="section-one-text">Високото качество на обучение в СофтУни е гарантирано от внимателно подбран преподавателски състав. Вярваме, че всяко дете заслужава обучители, които да го подкрепят и мотивират! Екипът ни се състои от професионалисти в сферата на програмирането, специалисти в работата с деца и мотивирани ментори.
+        </p>
+        </section>
+        <section className="section-two">
+            <article className="first-circle">
+                <img className="first-circle-img" src="VqraBogateva.jpg" alt=""/>
+            </article>
+            <article className="first-circle-text">
+                <h3>Вяра Богатева</h3>
+                <p className="h4">Образователен Директор</p>
+                <p>Д-р Вяра Богатева е Образователен Директор на SoftUni Kids. Тя е с докторска степен в катедра по предучилищно образование на СУ „Св. Климент Охридски” и е завършила специален изследователски курс в докторантската програма на Института по образованието - Университет в Лондон. Преподавател над 3 години в Софийския университет в областта на теорията и технологиите на семейното образование. Завършила е магистърска степен по предучилищна и начална училищна педагогика и бакалавърска степен по специална педагогика. Вяра Богатева е опитен учител с 10 години работа в областта на педагогиката, половината от които като мениджър на Интернационални институции.
+                </p>
+                </article>
+                <article className="second-circle">
+                    <img className="second-circle-img" src="VladimirNikolov.png" alt=""/>
+                </article>
+                <article className="second-circle-text">
+                    <h3>Владимир Николов</h3>
+                <p className="h4">Ръководител</p>
+                <p>Владимир Николов е ръководител на екип и преподавател в SoftUni Kids. По образование е културен антрополог, а по професия начален учител и педагог. Неговият опит за работа с деца и юноши започва от 2015 година до момента. Работил е в различни организации от неправителствения сектор и държавни училища, като винаги за него цел е било иновативната образователната промяна и даването на качествено и достъпно образование, както на децата, така и на хората работещи с тях. Именно тази мотивация и опит го прави част от инициативата SoftUni Kids, където вижда възможност да допринесе за подготовката на новото поколение на България за предизвикателствата на 21-ви век.
+                </p>
+                </article>
+            </section>
+
+
+            <section className="section-two-second">
+                <article className="section-two-second-img">
+                    <img className="section-two-second-img1" src="ElenaStamenova.jpg" alt=""/>
+                    <article className="section-two-second-text">
+                        <h3>Елена Стаменова</h3>
+                        <p className="h4">Преподавател</p>
+                    </article>
+                </article>
+
+
+                
+                    <article className="section-two-second-img">
+                        <img className="section-two-second-img2" src="SimeonSheitanov.jpg" alt=""/>
+                        <article className="section-two-second-text">
+                            <h3>Симеон Шейтанов</h3>
+                            <p className="h4">Преподавател</p>
+                        </article>
+                    </article>
+                
+
+                   
+                        <article className="section-two-second-img">
+                        <img className="section-two-second-img3" src="NadyaPetrova.jpg" alt=""/>
+                        <article className="section-two-second-text">
+                            <h3 className="small">Надя Петрова</h3>
+                            <p className="h4">Преподавател</p>
+                        </article>
+                    </article>
+
+
+                    
+                        <article className="section-two-second-img">
+                        <img className="section-two-second-img4" src="ElenaBarutchieva.jpg" alt=""/>
+                        <article className="section-two-second-text">
+                            <h3>Елена Барутчиева</h3>
+                            <p className="h4">Преподавател</p>
+                        </article>
+                    </article>
+        </section>
+       
+        </main>
+);
+}
+export default Main;
