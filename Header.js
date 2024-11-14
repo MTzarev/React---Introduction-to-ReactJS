07c5148aabd8536890991118be2a27af740ce71e
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-14 16:16:13 +0200

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,18 @@
+
+function Header(){
+return(
+    <header>
+    <img className="kids-logo" src="/kids-logo-white.svg" alt=""/>
+     <nav className="header-nav">
+         <ul>
+             <li><a href="">Обучения</a></li>
+             <li><a href="">Събития</a></li>
+             <li><a href="">Преподаватели</a></li>
+             <li><a href="">Блог</a></li>
+             <li><a href="">Контакти</a></li>
+         </ul>
+     </nav>
+ </header>
+);
+}
+export default Header;
