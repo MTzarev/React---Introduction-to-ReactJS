5545951f1d5c9a59e957aae90fa3ca31c5a0397f
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-14 16:15:54 +0200

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,35 @@
+function Footer(){
+return(
+    <footer>
+    <section class="footer-section">
+    <article class="softuni-kids">
+        <h4>SoftUni Kids</h4>
+        <p><a href="">Начало</a></p>
+        <p><a href="">Обучения</a></p>
+        <p><a href="">Учебни зали</a></p>
+    </article>
+    <article class="Всички обучения">
+        <h4>Всички обучения</h4>
+        <p><a href="">Курсове</a></p>
+        <p><a href="">Събития</a></p>
+    </article>
+    <article class="Всички Инициативи">
+        <h4>Всички инициативи</h4>
+        <p><a href="">Software University</a></p>
+        <p><a href="">SoftUni Digital</a></p>
+        <p><a href="">SoftUni Foundation</a></p>
+    </article>
+    <article class="links">
+        <h4>Последвайте ни</h4>
+        <a href="https://www.facebook.com/">
+        <i class="fa-brands fa-facebook-f"></i></a>
+       <a href=""> <i class="fa-brands fa-instagram"></i>
+       </a>
+    </article>
+</section>
+</footer>
+
+);
+
+}
+export default Footer;
